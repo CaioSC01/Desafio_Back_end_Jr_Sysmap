b6afc0306037e470f5ba6f8ed88bf41990552e3e
CaioSC01 <caio180g@gmail.com>
2023-02-03 16:11:55 -0300

    Atualização Banco de dados

--- Desafio Csharp/Inicio/Gerenciamento/Gerenciamento/Data/BancoContext.cs
+++ Desafio Csharp/Inicio/Gerenciamento/Gerenciamento/Data/BancoContext.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Gerenciamento.Data
 {
-    public class BancoContext: DbContext
+    public class BancoContext : DbContext
     {
         //o options passa minhas informções para dentro do Dbcontext 
         public BancoContext(DbContextOptions<BancoContext> options) : base(options)
@@ -16,5 +16,6 @@ namespace Gerenciamento.Data
         }
 
         public DbSet<TarefaModel> Tarefas { get; set; }
+        public DbSet<UsuarioModel> Usuarios { get; set; }
     }
 }
